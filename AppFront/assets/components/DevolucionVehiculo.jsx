eab0f610a9fbb69de6d87ed26c5b94594b5995fc
alejomj19971 <amuneraja@cesde.net>
2023-06-13 15:12:28 -0500

    si por favor

---
+++ AppFront/assets/components/DevolucionVehiculo.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class DevolucionVehiculo extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Componente devolución de vehiculo</Text>
+            </View>
+        )
+    }
+}
+
+export default DevolucionVehiculo
